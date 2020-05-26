dcabd775aa6a6f1892702f378e7dd86cca251ff7
Valentin B <valentin.be@protonmail.com>
2020-05-27 00:17:36 +0200

    Minecraftify all Read/Write types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -14,7 +14,7 @@ pub use self::byte::Byte;
 /// Readers are meant to be the primary layer for directly processing data received from an
 /// arbitrary input source to ensure integrity of the data into applications in a safe and
 /// strictly typed manner.
-pub trait PacketReader: Read {
+pub trait PacketReader {
     /// Reads a [`Bool`] from an underlying source of data.
     ///
     /// [`Bool`]: struct.Bool.html
@@ -26,6 +26,22 @@ pub trait PacketReader: Read {
     fn read_byte(&mut self) -> Result<Byte>;
 }
 
+impl PacketReader for dyn Read {
+    fn read_bool(&mut self) -> Result<Bool> {
+        let mut buf = [0; 1];
+        self.read(&mut buf)?;
+
+        Ok(Bool::from(buf[0]))
+    }
+
+    fn read_byte(&mut self) -> Result<Byte> {
+        let mut buf = [0; 1];
+        self.read(&mut buf)?;
+
+        Ok(Byte::from(buf[0] as i8))
+    }
+}
+
 /// A trait for parsing and writing Rust abstractions over the Minecraft protocol types to
 /// an underlying source of serialized binary data.
 ///
@@ -35,16 +51,30 @@ pub trait PacketReader: Read {
 ///
 /// Writers are meant to be the layer for directly processing application data into
 /// serialized and correct binary data which can be transferred over the network.
-pub trait PacketWriter: Write {
+pub trait PacketWriter {
     /// Writes a [`Bool`] to an underlying source of data.
     ///
+    /// Returns how many bytes were written. (`1`)
+    ///
     /// [`Bool`]: struct.Bool.html
-    fn write_bool(&mut self, b: Bool) -> Result<()>;
+    fn write_bool(&mut self, b: Bool) -> Result<usize>;
 
     /// Writes a [`Byte`] to an underlying source of data.
     ///
+    /// Returns how many bytes were written. (`1`)
+    ///
     /// [`Byte`]: struct.Byte.html
-    fn write_byte(&mut self, b: Byte) -> Result<()>;
+    fn write_byte(&mut self, b: Byte) -> Result<usize>;
+}
+
+impl PacketWriter for dyn Write {
+    fn write_bool(&mut self, b: Bool) -> Result<usize> {
+        self.write(&[b as u8])
+    }
+
+    fn write_byte(&mut self, b: Byte) -> Result<usize> {
+        self.write(&[b as u8])
+    }
 }
 
 mod bool;
